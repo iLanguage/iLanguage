8ff8d71e79061fbaa3ed6775d6b01ed58842f115
cesine <cesine@02662d70-ce12-4656-a2b7-ca9d6ff03df3>
2010-06-02 01:47:26 +0000

    Added directory structure
    git-svn-id: https://morphoparser.svn.sourceforge.net/svnroot/morphoparser@6 02662d70-ce12-4656-a2b7-ca9d6ff03df3
    
    Former-commit-id: 6f77d1ab7fae03df093dc09f8e7af7d94bfad6f5

---
+++ archive/Calculator/src/CalculatorProject.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : Calculator.cpp
+// Author      : gina
+// Version     :
+// Copyright   : CalculatorAndDataStructure
+// Description : Advanced Programming Project
+/*
+ * Goals: 	Create a command line user interface
+ * 			Demonstrate understanding of destructors in C++
+ * 			Demonstrate understanding of pointers and pointer manipulation in C++
+ *
+ * Included Files:
+ * 	CalculatorProject.cpp - User Interface and Presentation Logic
+ * 	LargeInt.cpp and .h - Class for arithmetic operations for use by the Calculator user interface
+ *  Sequence.cpp and .h - Class for dynamically allocating an arbitrarily large integer for use by the LargeInt class
+ */
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "It's working. " << endl;
+	return 0;
+}
---
+++ archive/Calculator/src/Sequence.cpp
@@ -0,0 +1,17 @@
+/*
+ * Sequence.cpp
+ *
+ *  Created on: Jun 1, 2010
+ *      Author: gina
+ */
+
+#include "Sequence.h"
+
+Sequence::Sequence() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Sequence::~Sequence() {
+	// TODO Auto-generated destructor stub
+}
