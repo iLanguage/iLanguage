3631a50d1a161a743007f4cc230d7a5cd8ac7f69
cesine <cesine@02662d70-ce12-4656-a2b7-ca9d6ff03df3>
2010-06-07 01:06:54 +0000

    integrated the addition function into the user interface logic (among other small tasks)
    git-svn-id: https://morphoparser.svn.sourceforge.net/svnroot/morphoparser@17 02662d70-ce12-4656-a2b7-ca9d6ff03df3

--- archive/Calculator/src/LargeInt.cpp
+++ archive/Calculator/src/LargeInt.cpp
@@ -23,6 +23,13 @@ LargeInt::LargeInt(char* characterInput):value(characterInput){
 	//if declared here rather than above in intialization list, the result is a local variable which is pretty useless.
 	//Sequence value2(characterInput);
 }
+
+void LargeInt::setValue(char* characterInput){
+	Sequence temp(characterInput);
+	value = temp;
+	if (trace) cout<<"Resetting the value to "<<characterInput<<endl;
+}
+
 LargeInt::LargeInt(const LargeInt & source): value(source.value){
 	if (trace) cout<<"Copying a  large int to a new one"<<endl;
 }
@@ -64,11 +71,19 @@ LargeInt& LargeInt::Add( LargeInt &input){
 	cout<<"Adding \n " <<holder<<endl<<"+"<<anotherHolder<<endl;
 
 
-	//TBD: check if need to add 1 in the front before adding +1 to the size
-	LargeInt result(x.getSize()+1);
-
-	//if the extra place value is not needed, set it to 0
+	//check if need a larger result to absorb the carry
+	int calculatedSize=0;
+	if(x.getElement(0)==0){
+		if (trace) cout<<"The longest operand already has enough space to absorb a carry, creating a result of the same size."<<endl;
+		calculatedSize = x.getSize();
+	}else{
+		if (trace) cout<<"Making a longer result array in case there is a carry."<<endl;
+		calculatedSize = x.getSize()+1;
+	}
+	LargeInt result(calculatedSize);
+	//in case the extra place value won't be used, initialize it to 0
 	result.value.setElement(0, 0);
+
 
 	int carry=0;
 	//for the places starting at zero, going until the size of the smaller operand
@@ -78,31 +93,34 @@ LargeInt& LargeInt::Add( LargeInt &input){
 		//calculate the addition in that position
 		carry = x.getElement(x.getSize()-i) + y.getElement(y.getSize()-i);
 
-		if(carry>baseSystem){
-				if (trace) cout<<"In this base system ("<<baseSystem<<") The addition of "<<x.getElement(x.getSize()-i)<<"+"<<y.getElement(y.getSize()-i)<<" results in a carry, here is the value: "<<carry<<"\t(Note: "
-						"\n\tthe carries are added to the x rather than to the result, so durring the "
-						"\n\tsuccessive carrying that might be triggered by a "
-						"\n\tfew 999 in a row the x changes, but not the resut holder)"<<endl;
-				//put the remainder into this position of the result
+		if(carry>=baseSystem){
+				if (trace) cout<<"In this base system ("<<baseSystem<<") The addition of "<<x.getElement(x.getSize()-i)<<"+"<<y.getElement(y.getSize()-i)<<" \tresults in a carry: "<<carry<<"\n\t(Note: "
+						"\n\tthe carries are added to the x rather than to the result, so if there are successive carries due to one addition, the x changes, but not the result holder)"<<endl;
+
+
 				result.value.setElement(result.getSize()-i, carry-baseSystem);
+				if(trace) cout<<" "<<i-1<<" place of result: "<<result.value.getElement(result.getSize()-i)<<endl;
 
 				//add one to the position to the left
 				int plusOne = 0;
 				plusOne =x.getElement(x.getSize()-i-1) +1;
 
-				int keepCarrying=1;
+				int keepCarrying=0;
 				//while the plusone is too big to fit in the position, send the carry further along to a higher position in x
 				while(plusOne>=baseSystem && (x.getSize()-keepCarrying-i >= 0) )
 				{
 					keepCarrying++;
-					if (trace) cout<<"Need to carry to the next position to the left."<<endl;
+					//if (trace) cout<<"Need to carry to the next position to the left."<<endl;
 					//put the remainder into the postion higher to the left
-					x.setElement(result.getSize()-i-keepCarrying, plusOne-baseSystem);
-					if (trace) cout<<"\t Now x looks like this: "<<x.getAsString()<<endl;
+					if(trace) cout<<" "<<i-1+keepCarrying<<" place of x was: "<<x.getElement(result.getSize()-i+1-keepCarrying)<<endl;
+					x.setElement(x.getSize()-i+1-keepCarrying, plusOne-baseSystem);
+					if(trace) cout<<" "<<i-1+keepCarrying<<" place of x is now: "<<x.getElement(result.getSize()-i+1-keepCarrying)<<endl;
+
+					//if (trace) cout<<"\t Now x looks like this: "<<x.getAsString()<<endl;
 
 					//carry again, add one to the next higher position
-					plusOne = x.getElement(x.getSize()-i-keepCarrying)+1;//move over to the left one spot each iteration
-					if (trace) cout<<"\t\t New carry: "<<plusOne<<endl;
+					plusOne = x.getElement(x.getSize()-i+1-keepCarrying)+1;//move over to the left one spot each iteration
+					if(trace) cout<<" "<<i-1+keepCarrying<<" place of x plus 1 would be: "<<plusOne<<endl;
 
 					//increment the number of times you carried
 
@@ -112,23 +130,27 @@ LargeInt& LargeInt::Add( LargeInt &input){
 
 				if( plusOne<baseSystem)// && x.getSize()-keepCarrying-i <= 0 ){
 				{
-					if(x.getSize()-keepCarrying-i <0)
+					if(x.getSize()-keepCarrying-i <=0)
 					{
 						//put the carry in the result so you dont have an array over load.
 						result.value.setElement(result.getSize()-i-keepCarrying, plusOne);
-						if (trace) cout <<"Putting the carry into the next higher position to the left in result. ["<<result.getSize()-i-keepCarrying<<"] "<<result.value.getElement(x.getSize()-i-keepCarrying)<<endl;
+						if(trace) cout<<"Reached the end of x\n\tPutting the carry into the padded area of the result instead.\n"
+								" "<<i-1+keepCarrying<<" place of the result: "<<result.value.getElement(result.getSize()-i-keepCarrying)<<endl;
 
-					}else{
-						x.setElement(x.getSize()-i-keepCarrying, plusOne);//equivalent to x.store[size-i+1]+=1; //add 1 to the higher place in the x, to be used in the next itteration of addition
-						if (trace) cout <<"Putting the carry into the next higher position to the left in x. ["<<x.getSize()-i-keepCarrying<<"] "<<x.getElement(x.getSize()-i-keepCarrying)<<endl;
+						//if (trace) cout <<"Putting the carry into the next higher position to the left in result. ["<<result.getSize()-i-keepCarrying<<"] "<<result.value.getElement(x.getSize()-i-keepCarrying)<<endl;
 					}
+
+					x.setElement(x.getSize()-i-keepCarrying, plusOne);//equivalent to x.store[size-i+1]+=1; //add 1 to the higher place in the x, to be used in the next itteration of addition
+					if(trace) cout<<"This is the value now in the "<<i+keepCarrying<<" place of x: "<<x.getElement(result.getSize()-i-keepCarrying)<<endl;
+					//if (trace) cout <<"Putting the carry into the next higher position to the left in x. ["<<x.getSize()-i-keepCarrying<<"] "<<x.getElement(x.getSize()-i-keepCarrying)<<endl;
+
 
 				}
 				if (trace) cout<<"\tCarried "<<keepCarrying<<" times."<<endl;
 		}else{
 				result.value.setElement(result.getSize()-i, carry);
 		}
-		if(trace) cout<<"This is the new value in the "<<i-1<<" place: "<<result.value.getElement(result.getSize()-i)<<endl;
+		//if(trace) cout<<"This is the new value in the "<<i-1<<" place: "<<result.value.getElement(result.getSize()-i)<<endl;
 	}
 
 	//for the higher digits of the longer opperand, copy the digits.
@@ -151,6 +173,17 @@ LargeInt& LargeInt::Add( LargeInt &input){
 
 
 }//end add
+
+/*
+LargeInt::Subtract(LargeInt& input){
+
+	if(trace) cout<<"First comparing numbers to see if the result is negative. "<<endl;
+	LargeInt result(12);
+
+	return result;
+}
+*/
+
 
 //LargeInt::LargeInt() {
 	// TODO Auto-generated constructor stub
--- archive/Calculator/src/LargeInt.h
+++ archive/Calculator/src/LargeInt.h
@@ -19,13 +19,16 @@ private:
 public:
 	//LargeInt();
 	LargeInt(char* characterInput);
+	void setValue(char* characterInput);//like a copy constructor, but takes only a cstring to reset the value of an existing object
 	explicit LargeInt (int n=10);
 	LargeInt(const LargeInt& source);//copy constructor
 	LargeInt& operator=(const LargeInt& source); //assignment operator overload
+
 	virtual ~LargeInt();
 	char* toString();
 
 	LargeInt& Add(LargeInt &input);
+	//LargeInt& Subtract(LargeInt &input);
 
 	int getSize();
 	char* getOriginalString();
