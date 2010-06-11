009c7a5940c60c80f5d79390455166c0f549d849
cesine <cesine@02662d70-ce12-4656-a2b7-ca9d6ff03df3>
2010-06-11 20:56:39 +0000

    Initial upload
    git-svn-id: https://morphoparser.svn.sourceforge.net/svnroot/morphoparser@23 02662d70-ce12-4656-a2b7-ca9d6ff03df3
    
    Former-commit-id: 0d0ec069a4b2ea8a710e287a6c6d04ade733052b

---
+++ archive/GeometricShapes/src/ShapeDisplayManager.h
@@ -0,0 +1,21 @@
+/*
+ * ShapeDisplayManager.h
+ *
+ *  Created on: Jun 10, 2010
+ *      Author: gina
+ */
+
+#ifndef SHAPEDISPLAYMANAGER_H_
+#define SHAPEDISPLAYMANAGER_H_
+
+class ShapeDisplayManager {
+public:
+	void run();
+	void printMenu();
+	void runInteractively();
+
+	ShapeDisplayManager();
+	virtual ~ShapeDisplayManager();
+};
+
+#endif /* SHAPEDISPLAYMANAGER_H_ */
---
+++ archive/GeometricShapes/src/ShapeView.cpp
@@ -0,0 +1,17 @@
+/*
+ * ShapeView.cpp
+ *
+ *  Created on: Jun 10, 2010
+ *      Author: gina
+ */
+
+#include "ShapeView.h"
+
+ShapeView::ShapeView() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ShapeView::~ShapeView() {
+	// TODO Auto-generated destructor stub
+}
