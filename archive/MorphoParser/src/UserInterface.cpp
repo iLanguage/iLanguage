c037a4bd66f2bd998d112f84b1ccc848baae44ff
cesine <cesine@02662d70-ce12-4656-a2b7-ca9d6ff03df3>
2010-07-05 23:52:57 +0000

    added menu item functionality
    git-svn-id: https://morphoparser.svn.sourceforge.net/svnroot/morphoparser@53 02662d70-ce12-4656-a2b7-ca9d6ff03df3
    
    Former-commit-id: 68e1f2051d9d10e2ff1090f0a3b3177c14020831

--- archive/MorphoParser/src/UserInterface.cpp
+++ archive/MorphoParser/src/UserInterface.cpp
@@ -10,22 +10,34 @@
 #include "NaturalLanguageMenuItem.h"
 #include <string>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 void UserInterface::run(){
 	runInteractively();
 }
 void UserInterface::printMainMenu(){
-	string menu= "Choose one of the following operations:\n"
-				"set language\n"
-				"quit \n";
-	Framer framedMenu(menu,"Main Menu","    ");
+	stringstream menu;
+	for(int i=0; i<menuList.size(); i++){
+		menu<<i<<" "<<menuList.at(i)->toString()<<"\n";
+	}
+	Framer framedMenu(menu.str(),"Main Menu","    ");
 	cout<<framedMenu.toString();
-	menuList.push_back(new NaturalLanguageMenuItem);
 
 }
 void UserInterface::runInteractively(){
-	printMainMenu();
+	bool stop=false;
+	while(!stop){
+		printMainMenu();
+		int menuNumber = getInt("Please enter a number corresponding to the menu item: ");
+		switch (menuNumber){
+		case 1:
+			cout<<"You choose 1";
+			stop=true;
+		default:
+			cout<<"Your menu choice was invalid."<<endl;
+		}//end switch
+	}//end while
 }
 void parseInput(){
 }
@@ -35,10 +47,30 @@ void addMenuItem(){
 void UserInterface::printLanguageMenu(){
 }
 UserInterface::UserInterface() {
-	// TODO Auto-generated constructor stub
-
+	menuList.push_back(new NaturalLanguageMenuItem("Quit","UserInterface"));
+	menuList.push_back(new NaturalLanguageMenuItem("Print","menu","UserInterface"));
 }
 
 UserInterface::~UserInterface() {
 	// TODO Auto-generated destructor stub
+}
+int UserInterface::getInt(string prompt) // definition of getInt(string) from FAQ
+{
+	cout << prompt;
+	int x;
+	cin >> x;
+	while ( ! cin )
+	{
+		cin.clear();
+		string badinput;
+		getline(cin, badinput);
+		cerr << "Expected an integer but found " << badinput << endl << prompt;
+		cin >> x;
+	}
+	// Optionally clear the remaining characters in the input stream;
+	// for example, for a user input '123xyz', 123 is extracted to x and,
+	// here we choose to discard 'xyz' which is still in the input stream
+	string leftover;
+	getline(cin, leftover);
+	return x; // ignore leftover
 }
