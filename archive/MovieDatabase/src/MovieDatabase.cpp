41c8a147cc823beb943b536aa955ed24b3538eb1
cesine <cesine@02662d70-ce12-4656-a2b7-ca9d6ff03df3>
2010-07-14 11:59:14 +0000

    added basic info for the movie class
    git-svn-id: https://morphoparser.svn.sourceforge.net/svnroot/morphoparser@55 02662d70-ce12-4656-a2b7-ca9d6ff03df3

---
+++ archive/MovieDatabase/src/MovieDatabase.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MovieDatabase.cpp
+// Author      : gina
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "It runs." << endl; // prints It runs.
+	return 0;
+}
