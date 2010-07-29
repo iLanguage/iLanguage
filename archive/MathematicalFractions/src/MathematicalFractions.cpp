ae4d913b68982a1b605ac85e213e0540fcca1812
cesine <cesine@02662d70-ce12-4656-a2b7-ca9d6ff03df3>
2010-07-29 20:06:25 +0000

    initial import
    git-svn-id: https://morphoparser.svn.sourceforge.net/svnroot/morphoparser@75 02662d70-ce12-4656-a2b7-ca9d6ff03df3
    
    Former-commit-id: ba652730b1e65ef7e0371fee59a78c752a75c96d

---
+++ archive/MathematicalFractions/src/MathematicalFractions.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MathematicalFractions.cpp
+// Author      : gina
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "It runs." << endl; // prints It runs.
+	return 0;
+}
