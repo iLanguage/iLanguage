05c18cc6f49c36c4bcc163662de46d6c8156d3a9
cesine <cesine@tower>
2012-05-12 11:24:42 -0400

    adding link for more info about the corpora

--- Corpora/get_corpora.sh
+++ Corpora/get_corpora.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#for more info http://research.ics.aalto.fi/events/morphochallenge2010/datasets.shtml
 wget http://research.ics.aalto.fi/events/morphochallenge2010/data/morphochal10data.zip
