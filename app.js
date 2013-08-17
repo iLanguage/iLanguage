15d58bb4fcf5dbc4c36d9fb72e032093206e8b2f
Pepe <aminesahibi@gmail.com>
2013-08-17 13:32:01 -0400

    Added posting performance

--- app.js
+++ app.js
@@ -4,10 +4,34 @@ var express = require('express'),
     http = require("http"),
     Q = require("q"),
     Sequelize = require("sequelize"),
-    performance = require("./lib/performance");
+    benchmarks = require ('./lib/benchmark'),
+    backends = require('./lib/backend'),
+    task = require ('./lib/task.js'),
+    command = require("./lib/command.js"),
+    performance = require("./lib/performance")
+    errors = require('./lib/errors')
+    appConfig = require('./appConfig.json');
     
 
 var type = grest.type;
+
+var options = {
+    dialect: "mysql",
+    storage: "./mcbench.sqlite"
+};
+
+process.argv.forEach(function (val) {
+    if (val === "--sqlite") {
+        options.dialect = "sqlite";
+    }
+});
+
+
+if (options.dialect === "sqlite") {
+    var sequelize = new Sequelize('mcbenchjs', 'root', 'root', options);
+} else if (options.dialect === "mysql") {
+    var sequelize = new Sequelize('mcbenchjs', 'root', 'root', {dialect: 'mysql'});
+}
 
 function toHttpResponse(value, res) {
     if (res === undefined) {
@@ -64,16 +88,12 @@ function toErrorLog(value) {
     }
 }
 
-var sequelize = new Sequelize('mcbenchjs', 'root', 'root', {
-	dialect: 'sqlite',
-	storage: './mcbench.sqlite'
-});
 performance.createDBSchema(sequelize, Sequelize);
 sequelize.sync();
 
 
 app.use(express.bodyParser());
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
 grest.rest(app, "", [
     "GET", ["test"], type.string,
     "Returns a test string",
@@ -85,7 +105,67 @@ grest.rest(app, "", [
     "Returns all performance results",
     function (req, res) {
         toHttpResponse(performance.getAll(), res);
-    }
+    },
+    "POST", ["performance", "run"], 
+    performance.schema,
+    type.array(task.schema),
+    "Creates performance tasks with the provided options. Return created tasks.", 
+    function (req,res) {
+        var benchmarkId = benchmarks.getId(req.body.benchmark);
+        var backendId = backends.getId(req.body.backend);
+        var scale = req.body.benchmark.scale;
+        var iteration = req.body.benchmark.iteration;
+        
+        toHttpResponse(
+            benchmarks
+            .get(benchmarkId)
+            .then(function (benchmark) {
+                return backends
+                .get(backendId)
+                .then(function (backend) {
+                    var sources = path.join(appConfig.benchmarks.path, benchmark.sources);
+                    var runPath = path.join( appConfig.benchmarks.path, benchmark.runPath);
+
+                    var t = taskManager.task(new command.Local(
+                        backends.operations[backendId].getCompileString({
+                            sources: sources,
+                            runPath: runPath
+                        }), 
+                        appConfig.command
+                    ));
+                    var t2 = taskManager.task(new command.Instrumented(new command.Local(
+                        backends.operations[backendId].getRunString({
+                            sources: sources,
+                            runPath: runPath
+                        }, [scale, iteration]), 
+                        appConfig.command
+                    )));
+
+                    toErrorLog(
+                        t.start()
+                        .then(t2)
+                        .then(function (t2) {
+                            return performance.add({
+                                benchmarkName: benchmark.name,
+                                benchmarkVersion: benchmark.version,
+                                backendName: backend.name,
+                                backendVersion: backend.version,
+                                compile:true,
+                                run:true,
+                                scale:scale,
+                                iteration:iteration,
+                                runtime:t2.command.runTime,
+                                startDate:t2.startTime,
+                                endDate:t2.endTime
+                            });
+                        })
+                    )
+                    return task.TasksToJS([t, t2]);
+                })
+            }),
+            res
+        );
+    },
 ]);
 
 http.createServer(app).listen(app.get('port'), function(){
