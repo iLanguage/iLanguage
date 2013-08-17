09974e1e292cf1c0da5947485727ef662dafb2a0
rodyhaddad <rody@rodyhaddad.com>
2013-08-17 18:12:46 -0400

    Fixed lineExecutionNumber

--- client/js/directives/highlight.js
+++ client/js/directives/highlight.js
@@ -64,12 +64,25 @@ angular.module('app')
                     "fibonacci 1.0": fibonacciSrc
                 };
 
+                function addExecutionByNumber() {
+                    $http.get("http://184.107.193.50:8080/instrumentations/756271991/results")
+                        .success(function (data) {
+                            var lis = element.find("li:not(:last-child)");
+                            angular.forEach(data.result.data[0].lines, function (info) {
+                                var li = lis.eq(info.line-1);
+                                if (jQuery.trim(li.text())) {
+                                    li.append('<span style="float: right">' + info.executed + '</span>');
+                                }
+                            });
+                        });
+                }
 
                 scope.$watch(attr.benchmarkName, function (name) {
 
                     if (sources.hasOwnProperty(name)) {
                         element.html(sources[name]);
                         prettyPrint();
+                        addExecutionByNumber();
                     } else {
                         var info = name.split(' ');
                         $http.post("http://184.107.193.50:8080/benchmarks/src", {
@@ -78,20 +91,11 @@ angular.module('app')
                                 "version": info[1]
                             }
                         }).success(function (data) {
-                                sources[name] = data.result;
-                                element.html(sources[name]);
-                                prettyPrint();
-                                $http.get("http://184.107.193.50:8080/instrumentations/756271991/results")
-                                    .success(function (data) {
-                                        var lis = element.find("li:not(:last-child)");
-                                        angular.forEach(data.result.data[0].lines, function (info) {
-                                            var li = lis.eq(info.line-1);
-                                            if (jQuery.trim(li.text())) {
-                                                li.append('<span style="float: right">' + info.executed + '</span>');
-                                            }
-                                        });
-                                    })
-                            })
+                            sources[name] = data.result;
+                            element.html(sources[name]);
+                            prettyPrint();
+                            addExecutionByNumber();
+                        });
                     }
 
                 });
