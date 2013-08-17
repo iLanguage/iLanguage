b425c04252d1ab186c39abf2633c8151ec0ed73d
cesine <cesine@yahoo.com>
2013-08-17 18:03:35 -0400

    added fibonacci

--- client/js/directives/highlight.js
+++ client/js/directives/highlight.js
@@ -3,11 +3,65 @@ angular.module('app')
         return {
             link: function (scope, element, attr) {
 
-                var fibonacciSrc = "";
+                var fibonacciSrc = ""
+				+"correct = [1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229 832040 1346269 2178309 3524578 5702887 9227465 14930352 24157817 39088169 63245986 102334155];"
+				+""
+				+"arg_list = argv();\n"
+				+"scale = str2num(arg_list{1});\n"
+				+"iteration = str2num(arg_list{2});\n"
+				+""
+				+"% Fibonacci.m by David Terr, Raytheon, 5-11-04\n"
+				+""
+				+"function fib = Fibonacci(n)\n"
+				+""
+				+"if ( n ~= floor(n) )\n"
+				+"    error('Argument must be an integer.');\n"
+				+"    return;\n"
+				+"end\n"
+				+""
+				+"if ~isreal(n)\n"
+				+"    error('Argument must be an integer.');\n"
+				+"    return;\n"
+				+"end\n"
+				+""
+				+"if size(n,1) ~= 1 || size(n,2) ~= 1\n"
+				+"    error('Argument must be an integer.');\n"
+				+"    return;\n"
+				+"end\n"
+				+""
+				+"if ( n == 0 )\n"
+				+"    fib = 0;\n"
+				+"    return;\n"
+				+"end\n"
+				+""
+				+"if ( n == 1 )\n"
+				+"    fib = 1;\n"
+				+"    return;\n"
+				+"end\n"
+				+""
+				+"if ( n < 0 )\n"
+				+"    fib = (-1)^(n+1) * Fibonacci(-n);\n"
+				+"    return;\n"
+				+"end\n"
+				+""
+				+"fib = Fibonacci(n-1) + Fibonacci(n-2);\n"
+				+""
+				+"end\n"
+				+"\n"
+				+"\n"
+				+"for n = 1:iteration\n"
+				+"    value = Fibonacci(scale);\n"
+				+"    if ((correct(scale) != value)) \n"
+				+"        exit(1);\n"
+				+"    end\n"
+				+"end\n"
+				+"exit(0);";
+
+
 
 
                 var sources = {
-                    //"fibonacci 1.0": fibonacciSrc
+                    "fibonacci 1.0": fibonacciSrc
                 };
 
 
