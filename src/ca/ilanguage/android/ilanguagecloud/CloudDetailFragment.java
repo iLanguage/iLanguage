4c3c92aee11d4c4adb653845dd2b10eddccb46f3
Joshua Horner <josh.horner@gmail.com>
2013-10-01 10:33:44 -0400

    Moved web view into a fragment.

--- src/ca/ilanguage/android/ilanguagecloud/CloudDetailFragment.java
+++ src/ca/ilanguage/android/ilanguagecloud/CloudDetailFragment.java
@@ -2,12 +2,19 @@ package ca.ilanguage.android.ilanguagecloud;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import ca.ilanguage.android.ilanguagecloud.dummy.DummyContent;
+import ca.ilanguage.android.wordcloud.AndroidWordCloudActivity.JavaScriptInterface;
 
 /**
  * A fragment representing a single Cloud detail screen. This fragment is either
@@ -15,6 +22,12 @@ import ca.ilanguage.android.ilanguagecloud.dummy.DummyContent;
  * {@link CloudDetailActivity} on handsets.
  */
 public class CloudDetailFragment extends Fragment {
+	
+	protected static final String TAG = "WordCloud";
+	public static final boolean D = true;
+	private String mOutPath;
+	private WebView mWebView;
+
 	/**
 	 * The fragment argument representing the item ID that this fragment
 	 * represents.
@@ -54,10 +67,69 @@ public class CloudDetailFragment extends Fragment {
 
 		// Show the dummy content as text in a TextView.
 		if (mItem != null) {
-			((TextView) rootView.findViewById(R.id.cloud_detail))
-					.setText(mItem.content);
+			mWebView = (WebView) rootView.findViewById(R.id.webView1);
+			mWebView.addJavascriptInterface(new JavascriptInterface(getActivity()),
+					"Android");
+			mWebView.setWebViewClient(new MyWebViewClient());
+			mWebView.setWebChromeClient(new MyWebChromeClient());
+			WebSettings webSettings = mWebView.getSettings();
+			webSettings.setBuiltInZoomControls(true);
+			// webSettings.setDefaultFontSize(26);
+			// webSettings.setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
+			webSettings.setJavaScriptEnabled(true);
+			/*
+			 * Let the webview handle persistance in form data
+			 */
+			// webSettings.setSaveFormData(true);
+
+			/*
+			 * Use HTML5 localstorage to maintain app state requires Android 2.1
+			 * or better, minsdk > 7
+			 */
+			webSettings.setDefaultTextEncodingName("utf-8");
+			webSettings.setAppCacheEnabled(true);
+			webSettings.setDomStorageEnabled(true);
+			webSettings.setDatabaseEnabled(true); // to use webSQL
+			webSettings.setDatabasePath(mOutPath + "databases/");
+			/*
+			 * Use HTML5 File API to read files
+			 */
+			webSettings.setAllowFileAccess(true);
+
+			webSettings.setLoadWithOverviewMode(true);
+			webSettings.setUseWideViewPort(true);
+			webSettings.setUserAgentString(webSettings.getUserAgentString()
+					+ " " + getString(R.string.app_name));
+
+			mWebView.loadUrl("file:///android_asset/wordcloud.html");
 		}
 
 		return rootView;
 	}
+	
+    class MyWebChromeClient extends WebChromeClient {
+   		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+  			mWebView.loadUrl("javascript:console.log('URL: " + url + "')");
+  			if(D) Log.d(TAG, "Overrode Url loading in WebChromeClient");
+  			return true;
+  		}
+
+  		@Override
+  		public boolean onConsoleMessage(ConsoleMessage cm) {
+  			if(D) Log.d(TAG, cm.message() + " -- From line " + cm.lineNumber() + " of "
+  					+ cm.sourceId());
+  			return true;
+  		}
+  	}
+
+  	class MyWebViewClient extends WebViewClient {
+  		@Override
+  		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+  			mWebView.loadUrl("javascript:console.log('URL: " + url + "')");
+  			if(D) Log.d(TAG, "Overrode Url loading in WebViewClient");
+  			return true;
+  		}
+
+  	}
+
 }
---
+++ src/ca/ilanguage/android/ilanguagecloud/JavascriptInterface.java
@@ -0,0 +1,20 @@
+package ca.ilanguage.android.ilanguagecloud;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class JavascriptInterface {
+
+		Context mContext;
+
+		/** Instantiate the interface and set the context */
+		JavascriptInterface(Context c) {
+			mContext = c;
+
+		}
+
+		@android.webkit.JavascriptInterface
+		public void showToast(String toast) {
+			Toast.makeText(mContext, toast, Toast.LENGTH_LONG).show();
+		}
+}
