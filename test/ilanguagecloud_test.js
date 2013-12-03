df38dd374e6088833434ec4058319df68a7e7b60
cesine <cesine@yahoo.com>
2013-12-03 16:38:41 -0500

    converting test to jasmine node

--- test/ilanguagecloud_test.js
+++ test/ilanguagecloud_test.js
@@ -1,36 +1,52 @@
 'use strict';
 
-var ilanguagecloud = require('../src/ilanguagecloud.js');
+var wordcloud = require('../src/ilanguagecloud.js');
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+  ======== A Handy Little Jasmine Reference ========
+https://github.com/pivotal/jasmine/wiki/Matchers
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
+  Spec matchers:
+    expect(x).toEqual(y); compares objects or primitives x and y and passes if they are equivalent
+    expect(x).toBe(y); compares objects or primitives x and y and passes if they are the same object
+    expect(x).toMatch(pattern); compares x to string or regular expression pattern and passes if they match
+    expect(x).toBeDefined(); passes if x is not undefined
+    expect(x).toBeUndefined(); passes if x is undefined
+    expect(x).toBeNull(); passes if x is null
+    expect(x).toBeTruthy(); passes if x evaluates to true
+    expect(x).toBeFalsy(); passes if x evaluates to false
+    expect(x).toContain(y); passes if array or string x contains y
+    expect(x).toBeLessThan(y); passes if x is less than y
+    expect(x).toBeGreaterThan(y); passes if x is greater than y
+    expect(function(){fn();}).toThrow(e); passes if function fn throws exception e when executed
+    
+    Every matcher's criteria can be inverted by prepending .not:
+    expect(x).not.toEqual(y); compares objects or primitives x and y and passes if they are not equivalent
+
+    Custom matchers help to document the intent of your specs, and can help to remove code duplication in your specs.
+    beforeEach(function() {
+      this.addMatchers({
+
+        toBeLessThan: function(expected) {
+          var actual = this.actual;
+          var notText = this.isNot ? " not" : "";
+    
+          this.message = function () {
+            return "Expected " + actual + notText + " to be less than " + expected;
+          }
+    
+          return actual < expected;
+        }
+
+      });
+    });
+
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(ilanguagecloud.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  }
-};
+
+describe("lib/wordcloud", function() {
+    it("should load", function() {
+        expect(wordcloud.awesome()).toBe('awesome');
+    });
+});
+
