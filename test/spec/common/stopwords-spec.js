1af5d794eaa1deec0f32961f041f4df9320d57de
Joshua Horner <josh.horner@gmail.com>
2013-12-13 12:29:38 -0500

    Move stopwords processor to its own class. Export that class and catch error for bad user-provided regexp. Include in tests.

--- test/spec/common/stopwords-spec.js
+++ test/spec/common/stopwords-spec.js
@@ -1,12 +1,25 @@
 'use strict';
+var StopWords = require('../../../src/common/stop-words');
 
 describe('lib/stopwords', function() {
 
+  it('should load', function() {
+    expect(StopWords).toBeDefined();
+  });
+
   it('should accept space seperated list', function() {
-    expect(true).toBeTruthy();
+    var spaceSeparatedList = 'a an the he she it we go be';
+    var result = StopWords.processStopWords({
+      stopWords: spaceSeparatedList
+    });
+    expect(result.stopWords).toEqual(/^(a|an|the|he|she|it|we|go|be)$/);
   });
 
   it('should accept a regular expression', function() {
+    expect(true).toBeTruthy();
+  });
+
+  it('should throw an error if an invalid regex is provided', function() {
     expect(true).toBeTruthy();
   });
 
