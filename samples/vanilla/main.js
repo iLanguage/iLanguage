f1f01743c0572232a1fa9ab3dce302e9b58757f0
cesine <cesine@yahoo.com>
2014-03-07 16:24:02 -0500

    created a lexicon using collections and rendering it in the page with frb

--- samples/vanilla/main.js
+++ samples/vanilla/main.js
@@ -2,7 +2,7 @@ var firstdb = "glossersample-quechua";
 var firstGlosser = new Glosser({
   pouchname: firstdb
 });
-firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(rules) {
+firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
   var utterance = firstGlosser.guessUtteranceFromMorphemes({
     utterance: "",
     morphemes: "Kicha-nay-wa-n punqo-ta",
@@ -11,21 +11,40 @@ firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb
     translation: "I feel like opening the door."
   });
   console.log(utterance);
-  firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(rules, document.getElementById("glosser"), firstdb, null, "dont connect on word boundaries so we can see templates in paradigms");
+  var lexicon = LexiconFactory({
+    precedenceRelations: precedenceRelations,
+    dbname: firstdb,
+    element: document.getElementById("lexicon"),
+    dontConnectWordBoundaries: true
+  });
+  lexicon.bindToView();
+  firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, document.getElementById("glosser"), firstdb, "dont connect on word boundaries so we can see templates in paradigms");
 });
 
 var seconddb = "glossersample-cherokee";
 var secondGlosser = new Glosser({
   pouchname: seconddb
 });
-secondGlosser.downloadPrecedenceRules(seconddb, "http://localhost:5984/" + seconddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(rules) {
-  secondGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(rules, document.getElementById("glosser2"), seconddb, document.getElementById("lexicon2"));
+secondGlosser.downloadPrecedenceRules(seconddb, "http://localhost:5984/" + seconddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
+  var lexicon = LexiconFactory({
+    precedenceRelations: precedenceRelations,
+    dbname: seconddb,
+    element: document.getElementById("lexicon2"),
+    dontConnectWordBoundaries: true
+  });
+  secondGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, document.getElementById("glosser2"), seconddb);
 });
 
 var thirddb = "glossersample-inuktitut";
 var thirdGlosser = new Glosser({
   pouchname: thirddb
 });
-thirdGlosser.downloadPrecedenceRules(thirddb, "http://localhost:5984/" + thirddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(rules) {
-  thirdGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(rules, document.getElementById("glosser3"), thirddb, null , "dont connect on word boundaries so we can see templates in paradigms");
+thirdGlosser.downloadPrecedenceRules(thirddb, "http://localhost:5984/" + thirddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
+  var lexicon = LexiconFactory({
+    precedenceRelations: precedenceRelations,
+    dbname: thirddb,
+    element: document.getElementById("lexicon3"),
+    dontConnectWordBoundaries: true
+  });
+  thirdGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, document.getElementById("glosser3"), thirddb, "dont connect on word boundaries so we can see templates in paradigms");
 });
