0bf89eefb5896c6c62848cd4fc5db4ad8b47aa1b
cesine <cesine@yahoo.com>
2014-03-07 01:09:06 -0500

    using new map reduce to have confidecne for morphemes

--- samples/vanilla/main.js
+++ samples/vanilla/main.js
@@ -2,7 +2,7 @@ var firstdb = "glossersample-quechua";
 var firstGlosser = new Glosser({
   pouchname: firstdb
 });
-firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb + "/_design/pages/_view/precedence_rules?group=true", function() {
+firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(rules) {
   var utterance = firstGlosser.guessUtteranceFromMorphemes({
     utterance: "",
     morphemes: "Kicha-nay-wa-n punqo-ta",
@@ -11,21 +11,21 @@ firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb
     translation: "I feel like opening the door."
   });
   console.log(utterance);
-  firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(null, document.getElementById("glosser"), firstdb);
+  firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(rules, document.getElementById("glosser"), firstdb, null, "dont connect on word boundaries so we can see templates in paradigms");
 });
 
 var seconddb = "glossersample-cherokee";
 var secondGlosser = new Glosser({
   pouchname: seconddb
 });
-secondGlosser.downloadPrecedenceRules(seconddb, "http://localhost:5984/" + seconddb + "/_design/pages/_view/precedence_rules?group=true", function() {
-  secondGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(null, document.getElementById("glosser2"), seconddb);
+secondGlosser.downloadPrecedenceRules(seconddb, "http://localhost:5984/" + seconddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(rules) {
+  secondGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(rules, document.getElementById("glosser2"), seconddb, document.getElementById("lexicon2"));
 });
 
 var thirddb = "glossersample-inuktitut";
 var thirdGlosser = new Glosser({
   pouchname: thirddb
 });
-thirdGlosser.downloadPrecedenceRules(thirddb, "http://localhost:5984/" + thirddb + "/_design/pages/_view/precedence_rules?group=true", function() {
-  thirdGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(null, document.getElementById("glosser3"), thirddb);
+thirdGlosser.downloadPrecedenceRules(thirddb, "http://localhost:5984/" + thirddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(rules) {
+  thirdGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(rules, document.getElementById("glosser3"), thirddb, null , "dont connect on word boundaries so we can see templates in paradigms");
 });
