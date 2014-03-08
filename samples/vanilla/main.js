4a1e7d1eec3daacb3ab19ff27900ca840c81c71b
cesine <cesine@yahoo.com>
2014-03-08 12:55:01 -0500

    connected switches to hide and show the glosser graph, hide and show the lexicon list and graph with and without word boundarires (with you can see the template of words, without you can see which morphemes are over represetented in the corpus) fixes OpenSourceFieldlinguistics/FieldDB#1409 and fixes OpenSourceFieldlinguistics/FieldDB#1409

--- samples/vanilla/main.js
+++ samples/vanilla/main.js
@@ -1,52 +1,116 @@
-var firstdb = "glossersample-quechua";
-var firstGlosser = new Glosser({
-  pouchname: firstdb
-});
-firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
-  var utterance = firstGlosser.guessUtteranceFromMorphemes({
-    utterance: "",
-    morphemes: "Kicha-nay-wa-n punqo-ta",
-    allomorphs: "",
-    gloss: "open-DES-1OM-3SG door-ACC",
-    translation: "I feel like opening the door."
-  });
-  console.log(utterance);
-  var lexicon = LexiconFactory({
-    precedenceRelations: precedenceRelations,
-    dbname: firstdb,
-    element: document.getElementById("lexicon"),
-    dontConnectWordBoundaries: true
-  });
-  lexicon.bindToView();
-  firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, document.getElementById("glosser"), firstdb, "dont connect on word boundaries so we can see templates in paradigms");
-});
+var loadExamples = function() {
+  var graphWordBoundaries = document.getElementById("graphWordBoundaries");
+  var showWordBoundaries;
+  if (graphWordBoundaries && graphWordBoundaries.checked) {
+    showWordBoundaries = true;
+  } else {
+    showWordBoundaries = false;
+  }
 
-var seconddb = "glossersample-cherokee";
-var secondGlosser = new Glosser({
-  pouchname: seconddb
-});
-secondGlosser.downloadPrecedenceRules(seconddb, "http://localhost:5984/" + seconddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
-  var lexicon = LexiconFactory({
-    precedenceRelations: precedenceRelations,
-    dbname: seconddb,
-    element: document.getElementById("lexicon2"),
-    dontConnectWordBoundaries: true
-  });
-  lexicon.bindToView();
-  secondGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, document.getElementById("glosser2"), seconddb, "dontusewordboundaries");
-});
+  var rerenderIfWordBoundariesChange = [];
+  graphWordBoundaries.onchange = function() {
+    if (graphWordBoundaries && graphWordBoundaries.checked) {
+      showWordBoundaries = true;
+    } else {
+      showWordBoundaries = false;
+    }
+    for (var functionToCall = 0; functionToCall < rerenderIfWordBoundariesChange.length; functionToCall++) {
+      rerenderIfWordBoundariesChange[functionToCall]();
+    }
+  };
 
-var thirddb = "glossersample-inuktitut";
-var thirdGlosser = new Glosser({
-  pouchname: thirddb
-});
-thirdGlosser.downloadPrecedenceRules(thirddb, "http://localhost:5984/" + thirddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
-  var lexicon = LexiconFactory({
-    precedenceRelations: precedenceRelations,
-    dbname: thirddb,
-    element: document.getElementById("lexicon3"),
-    dontConnectWordBoundaries: true
+  var firstdb = "glossersample-quechua";
+  var firstGlosser = new Glosser({
+    pouchname: firstdb
   });
-  lexicon.bindToView();
-  thirdGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, document.getElementById("glosser3"), thirddb, "dont connect on word boundaries so we can see templates in paradigms");
-});
+  firstGlosser.downloadPrecedenceRules(firstdb, "http://localhost:5984/" + firstdb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
+    var utterance = firstGlosser.guessUtteranceFromMorphemes({
+      utterance: "",
+      morphemes: "Kicha-nay-wa-n punqo-ta",
+      allomorphs: "",
+      gloss: "open-DES-1OM-3SG door-ACC",
+      translation: "I feel like opening the door."
+    });
+    console.log(utterance);
+    var lexicon = LexiconFactory({
+      precedenceRelations: precedenceRelations,
+      dbname: firstdb,
+      element: document.getElementById("lexicon"),
+      dontConnectWordBoundaries: !showWordBoundaries
+    });
+    lexicon.bindToView();
+    var renderFirstGraph = function() {
+      var glosserElement = document.getElementById("glosser");
+      glosserElement.innerHTML = "";
+      firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, glosserElement, !showWordBoundaries);
+    };
+    rerenderIfWordBoundariesChange.push(renderFirstGraph);
+    renderFirstGraph();
+  });
+
+  var seconddb = "glossersample-cherokee";
+  var secondGlosser = new Glosser({
+    pouchname: seconddb
+  });
+  secondGlosser.downloadPrecedenceRules(seconddb, "http://localhost:5984/" + seconddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
+    var lexicon = LexiconFactory({
+      precedenceRelations: precedenceRelations,
+      dbname: seconddb,
+      element: document.getElementById("lexicon2"),
+      dontConnectWordBoundaries: !showWordBoundaries
+    });
+    lexicon.bindToView();
+
+    var renderSecondGraph = function() {
+      var glosserElement = document.getElementById("glosser2");
+      glosserElement.innerHTML = "";
+      firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, glosserElement, !showWordBoundaries);
+    };
+    rerenderIfWordBoundariesChange.push(renderSecondGraph);
+    renderSecondGraph();
+  });
+
+  var thirddb = "glossersample-inuktitut";
+  var thirdGlosser = new Glosser({
+    pouchname: thirddb
+  });
+  thirdGlosser.downloadPrecedenceRules(thirddb, "http://localhost:5984/" + thirddb + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true", function(precedenceRelations) {
+    var lexicon = LexiconFactory({
+      precedenceRelations: precedenceRelations,
+      dbname: thirddb,
+      element: document.getElementById("lexicon3"),
+      dontConnectWordBoundaries: !showWordBoundaries
+    });
+    lexicon.bindToView();
+
+    var renderThirdGraph = function() {
+      var glosserElement = document.getElementById("glosser3");
+      glosserElement.innerHTML = "";
+      firstGlosser.visualizePrecedenceRelationsAsForceDirectedGraph(lexicon, glosserElement, !showWordBoundaries);
+    };
+    rerenderIfWordBoundariesChange.push(renderThirdGraph);
+    renderThirdGraph();
+  });
+
+};
+
+var refreshButton = document.getElementById("refreshButton");
+refreshButton.onclick = loadExamples;
+
+
+var showGlosserAsGraph = document.getElementById("showGlosserAsGraph");
+showGlosserAsGraph.onchange = function(){
+  document.getElementById("glosser").hidden = !showGlosserAsGraph.checked;
+  document.getElementById("glosser2").hidden = !showGlosserAsGraph.checked;
+  document.getElementById("glosser3").hidden = !showGlosserAsGraph.checked;
+};
+
+
+var showLexiconAsList = document.getElementById("showLexiconAsList");
+showLexiconAsList.onchange = function(){
+  document.getElementById("lexicon").hidden = !showLexiconAsList.checked;
+  document.getElementById("lexicon2").hidden = !showLexiconAsList.checked;
+  document.getElementById("lexicon3").hidden = !showLexiconAsList.checked;
+};
+
+loadExamples();
