170c021735e584433c4416b66ef638e3ddb867b4
cesine <cesine@yahoo.com>
2014-05-30 11:22:26 +0400

    setting a limit 10 most recent items
    Former-commit-id: 704eb53a9875b6d91ff5b3c7afb9a222843a7301

--- src/common/database/Database.js
+++ src/common/database/Database.js
@@ -9,7 +9,7 @@
     FieldDBObject.apply(this, arguments);
   };
 
-  var DEFAULTALLDOCUMENTSMAPREDUCE = '_design/pages/_view/datums';
+  var DEFAULTALLDOCUMENTSMAPREDUCE = '_design/pages/_view/datums?descending=true&limit=10';
   Database.prototype = Object.create(FieldDBObject.prototype, /** @lends Database.prototype */ {
     constructor: {
       value: Database
