7356aaff4033217ba199f2fc64c15306c2dceff3
cesine <cesine@yahoo.com>
2015-06-08 21:36:46 -0400

    yo node-gulp --test-framework=jasmine

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+
+* v1.0 April 16 2009 
+  - Initial implementation in bash and perl
+
+* v2.0 Jul 3 2010 
+  - Implementation in C++
+
+* v3.0 April 30 2011 
+  - Implementation in Groovy 
+
+* v4.0 July 20 2012 
+  - Implementation in JavaScript Map Reduce
+
+* v4.1 Nov 29 2013 
+  - Added more high level functions for gloss lookup
+
+* v5.0 Jan 9 2014 
+  - Implementation in CommonJS
+
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var gulp   = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var paths = {
+  jshint: ['./gulpfile.js'],
+  watch: ['./gulpfile.js', './js/**', './test/**/*.js', '!test/{temp,temp/**}'],
+  tests: ['./test/**/*-spec.js', '!test/{temp,temp/**}'],
+  source: ['./js/*.js']
+};
+
+var plumberConf = {};
+
+if (process.env.CI) {
+  plumberConf.errorHandler = function(err) {
+    throw err;
+  };
+}
+
+gulp.task('jshint', function () {
+  return gulp.src(paths.jshint)
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.plumber(plumberConf))
+    .pipe(plugins.jscs())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('istanbul', function (cb) {
+  gulp.src(paths.source)
+    .pipe(plugins.istanbul()) // Covering files
+    .pipe(plugins.istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', function () {
+      gulp.src(paths.tests)
+        .pipe(plugins.plumber(plumberConf))
+        .pipe(plugins.jasmine())
+        .pipe(plugins.istanbul.writeReports()) // Creating the reports after tests runned
+        .on('finish', function() {
+          process.chdir(__dirname);
+          cb();
+        });
+    });
+});
+
+gulp.task('bump', ['test'], function () {
+  var bumpType = plugins.util.env.type || 'patch'; // major.minor.patch
+
+  return gulp.src(['./package.json'])
+    .pipe(plugins.bump({ type: bumpType }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', ['test'], function () {
+  gulp.watch(paths.watch, ['test']);
+});
+
+gulp.task('test', ['jshint', 'istanbul']);
+
+gulp.task('release', ['bump']);
+
+gulp.task('default', ['test']);
